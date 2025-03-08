855e53aaef860ccbbe1503ff190ff18a7a377dbd
mohit1721 <mohitmandal192@gmail.com>
2025-03-08 11:42:16 +0530

    docker added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Make sure the app is accessible outside of the container
+    port: 5173,      // Use port 5173, as you mentioned
+  },
 })
